12fcc29c23cccd87dd4469b9cdb852e73c54aa6b
Brittany Yoon <binna.yoon@gmail.com>
2013-01-15 21:07:16 -0800

    Code reviewed by Brittany

--- minesweeper.rb
+++ minesweeper.rb
@@ -14,14 +14,14 @@ class Minesweeper
     size = gets.chomp.to_i
     unless size == 1 || size == 2
       puts "invalid entry"
-      user_settings
+      user_settings #BY I'm sure you remember but Ned discouraged recursive loop for error 
     end
     size
   end
 
   def play
     build_gameboard(user_settings)
-    while game_over == false
+    while game_over == false #or while !game_over
       print_board
       player_move = move
       check_coordinate(player_move)
@@ -29,17 +29,17 @@ class Minesweeper
     "GAME"
   end
 
-  def game_over
+  def game_over #should be game_over? 
     false
   end
 
   # gameboard methods
   def build_gameboard(size)
     n, m = 0
-    n, m = 9, 10 if size == 1
+    n, m = 9, 10 if size == 1 #I like that user can change game board size
     n, m = 16, 40 if size == 2
     @gameboard = Array.new(n) do
-      Array.new(n) { Square.new }
+      Array.new(n) { Square.new } #I like that you pulled tile as its own class
     end
     #debugger
     set_bomb(m)
@@ -49,8 +49,8 @@ class Minesweeper
   def set_bomb(bomb_count)
     bombs = 0
     while true
-      return if bombs == bomb_count
-      row = @gameboard.sample
+      return if bombs == bomb_count #or unless bomb == bomb_count
+      row = @gameboard.sample #this is a cool method!
       square = row.sample
       if square.bomb == false
         square.bomb = true
@@ -82,11 +82,11 @@ class Minesweeper
     end
   end
 
-  def fringe_sq_iterator
+  def fringe_sq_iterator #unclear name for a method
     @gameboard.each_with_index do |row, i|
       row.each_with_index do |square, j|
-        if square.bomb == true
-          adjacents = adjacent_squares([i, j])
+        if square.bomb == true #if square.bomb
+          adjacents = adjacent_squares([i, j]) 
           adjacents.each do |coordinates|
             @gameboard[coordinates[0]][coordinates[1]].adj_bomb += 1
           end
@@ -101,7 +101,7 @@ class Minesweeper
       y = coord[1] + coordinates[1]
       [x, y]
     end
-    selected = adjacents.select do |coord|
+    selected = adjacents.select do |coord| #instead of selecting those that meet the categories you could check in the mapping
       coord[0] < @gameboard.length && coord[0] > 0 && coord[1] < @gameboard.length && coord[1] > 0
     end
     selected
@@ -110,7 +110,7 @@ class Minesweeper
   # user methods
 
   def move
-    puts "Type 'R' and your coordinates to reveal (ex: R 4 5)"
+    puts "Type 'R' and your coordinates to reveal (ex: R 4 5)" #probably made life easier for you guys to do numbers on x and y cord!
     puts "Type 'F' and your coordinates to flag (ex: F 4 5)"
     move = gets.chomp.split(' ')
     [move[0].downcase, [move[1].to_i, move[2].to_i]]
@@ -126,7 +126,7 @@ class Minesweeper
       else
         reveal(coord)
       end
-    elsif type == 'f'
+    elsif type == 'f' #could just be else
       @gameboard[coord[0]][coord[1]].flagged = true
     end
   end
@@ -136,7 +136,7 @@ class Minesweeper
     queue = [move]
     checked = []
 
-    while move = queue.shift
+    while move = queue.shift #we did this recursively, but this makes sense. 
       @gameboard[move[0]][move[1]].revealed = true
       checked << move
       to_check = adjacent_squares(move)
